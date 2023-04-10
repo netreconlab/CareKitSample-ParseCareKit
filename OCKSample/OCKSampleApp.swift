0998a8a340dae2098aeed0bf5044b35396315a33
Corey <coreyearleon@icloud.com>
2023-04-10 19:18:08 -0400

    move sync properties

--- OCKSample/OCKSampleApp.swift
+++ OCKSample/OCKSampleApp.swift
@@ -10,6 +10,19 @@ import Foundation
 import SwiftUI
 import CareKit
 
+/**
+ Set to **true** to sync with ParseServer, *false** to sync with iOS/watchOS.
+ */
+let isSyncingWithCloud = true
+
+/**
+ Set to **true** to use WCSession to notify watchOS about updates, **false** to not notify.
+ A change in watchOS 9 removes the ability to use Websockets on real Apple Watches,
+ preventing auto updates from the Parse Server. See the link for
+ details: https://developer.apple.com/forums/thread/715024
+ */
+let isSendingPushUpdatesToWatch = true
+
 @main
 struct OCKSampleApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
