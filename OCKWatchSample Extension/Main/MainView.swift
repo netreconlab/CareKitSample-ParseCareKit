56fd4b2eb404f04402051bd4e536619e2689f362
Corey Baker <coreyearleon@icloud.com>
2025-04-30 16:31:58 -0700

    remove unecessary navigation stack

--- OCKWatchSample Extension/Main/MainView.swift
+++ OCKWatchSample Extension/Main/MainView.swift
@@ -15,32 +15,24 @@ import os.log
 struct MainView: View {
     @EnvironmentObject private var appDelegate: AppDelegate
     @StateObject private var loginViewModel = LoginViewModel()
-    @State private var path = [MainViewPath]()
     @State private var storeCoordinator = OCKStoreCoordinator()
+	@State private var isLoggedIn: Bool?
 
     var body: some View {
-        NavigationStack(path: $path) {
-            LoginView(viewModel: loginViewModel)
-                .navigationDestination(for: MainViewPath.self) { destination in
-                    switch destination {
-                    case .tabs:
-						MainTabView(loginViewModel: loginViewModel)
-                            .navigationBarHidden(true)
-                    }
-                }
-                .navigationBarHidden(true)
-                .onAppear {
-                    guard isSyncingWithRemote else {
-                        updatePath([.tabs])
-                        return
-                    }
-                    guard !loginViewModel.isLoggedOut else {
-                        updatePath([])
-                        return
-                    }
-                    updatePath([.tabs])
-                }
+        Group {
+			if let isLoggedIn {
+				if isLoggedIn {
+					MainTabView(loginViewModel: loginViewModel)
+				} else {
+					LoginView(viewModel: loginViewModel)
+				}
+			} else {
+				Text("Loading...")
+			}
         }
+		.task {
+			await loginViewModel.checkStatus()
+		}
 		.environment(\.careStore, storeCoordinator)
 		.onReceive(appDelegate.$storeCoordinator) { newStoreCoordinator in
 			Task {
@@ -50,25 +42,12 @@ struct MainView: View {
 				return
 			}
 			storeCoordinator = newStoreCoordinator
-			updatePath([.tabs])
 		}
-        .onReceive(loginViewModel.$isLoggedOut) { isLoggedOut in
-            guard isSyncingWithRemote else {
-                updatePath([.tabs])
-                return
-            }
-            guard !isLoggedOut else {
-                updatePath([])
-                return
-            }
-            updatePath([.tabs])
+		.onReceive(loginViewModel.isLoggedIn.publisher) { currentStatus in
+			isLoggedIn = currentStatus
         }
     }
 
-    @MainActor
-    func updatePath(_ path: [MainViewPath]) {
-        self.path = path
-    }
 }
 
 struct MainView_Previews: PreviewProvider {
